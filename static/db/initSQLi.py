c4b059c9d482b09038cecb6a0a45b28f8e01451e
eserverhn <126129272+eserverhn@users.noreply.github.com>
2023-03-04 20:04:55 -0600

    Actualizaciiones varias a los módulos SQLi

--- static/db/initSQLi.py
+++ static/db/initSQLi.py
@@ -49,10 +49,7 @@ cur.execute("INSERT INTO SALON (id, nombre, apellido, calificacion) VALUES (?,?,
 cur.execute("INSERT INTO SALON (id, nombre, apellido, calificacion) VALUES (?,?,?,?)",
             (5, 'Wolfgang','Mozart','100'))
 cur.execute("INSERT INTO SALON (id, nombre, apellido, calificacion) VALUES (?,?,?,?)",
-            (6, 'John','Williams','100'))
-cur.execute("INSERT INTO SALON (id, nombre, apellido, calificacion) VALUES (?,?,?,?)",
-            (108021932, 'Esta','es la clave','7dff51ca8eb990122513f24ffdaa4d9a'))
-
+            (6, 'John','Williams','100'))   
 connection.commit()
 connection.close()
 
@@ -61,26 +58,26 @@ connection.close()
 connection = sqlite3.connect('SQLI03.db')
 
 
-with open('schemaSQLi.sql') as f:
+with open('schemaSQLi03.sql') as f:
     connection.executescript(f.read())
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO SALON (id, nombre, apellido, calificacion) VALUES (?,?,?,?)",
-            (1, 'Juan','Bach','95'))
+cur.execute("INSERT INTO SALON (id, nombre, apellido, calificacion, ciudad, pais, tesoro) VALUES (?,?,?,?,?,?,?)",
+            (1, 'Juan','Bach','95','turingia','alemania','nada'))
 
-cur.execute("INSERT INTO SALON (id, nombre, apellido, calificacion) VALUES (?,?,?,?)",
-            (2, 'Ludwig','Bethoven','98'))
-cur.execute("INSERT INTO SALON (id, nombre, apellido, calificacion) VALUES (?,?,?,?)",
-            (3, 'Federico','Chopin','96'))
-cur.execute("INSERT INTO SALON (id, nombre, apellido, calificacion) VALUES (?,?,?,?)",
-            (4, 'Jorge','Handel','95'))
-cur.execute("INSERT INTO SALON (id, nombre, apellido, calificacion) VALUES (?,?,?,?)",
-            (5, 'Wolfgang','Mozart','100'))
-cur.execute("INSERT INTO SALON (id, nombre, apellido, calificacion) VALUES (?,?,?,?)",
-            (6, 'John','Williams','100'))
-cur.execute("INSERT INTO SALON (id, nombre, apellido, calificacion) VALUES (?,?,?,?)",
-            (27011756, 'Esta','es la clave','738c8372fab9160336f3daad7fcc7e2'))
+cur.execute("INSERT INTO SALON (id, nombre, apellido, calificacion, ciudad, pais, tesoro) VALUES (?,?,?,?,?,?,?)",
+            (2, 'Ludwig','Bethoven','98','bonn','alemania', 'nada'))
+cur.execute("INSERT INTO SALON (id, nombre, apellido, calificacion, ciudad, pais, tesoro) VALUES (?,?,?,?,?,?,?)",
+            (3, 'Federico','Chopin','96','varsovia','polonia','nada'))
+cur.execute("INSERT INTO SALON (id, nombre, apellido, calificacion, ciudad, pais, tesoro) VALUES (?,?,?,?,?,?,?)",
+            (4, 'Jorge','Handel','95','halle','alemania','nada'))
+cur.execute("INSERT INTO SALON (id, nombre, apellido, calificacion, ciudad, pais, tesoro) VALUES (?,?,?,?,?,?,?)",
+            (5, 'Wolfgang','Mozart','100','salsburgo','austria','nada'))
+cur.execute("INSERT INTO SALON (id, nombre, apellido, calificacion, ciudad, pais, tesoro) VALUES (?,?,?,?,?,?,?)",
+            (6, 'John','Williams','100','nueva york','eeuu', 'nada'))
+cur.execute("INSERT INTO SALON (id, nombre, apellido, calificacion, ciudad, pais, tesoro) VALUES (?,?,?,?,?,?,?)",
+            (27011756, 'columna 5 - ciudad','columna 6 - pais','columna 7 - tesoro','Esta es','la clave','738c8372fab9160336f3daad7fcc7e2a'))
 
 connection.commit()
 connection.close()
---
+++ static/db/schemaSQLi03.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS SALON;
+
+CREATE TABLE SALON (
+    id INTEGER PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    apellido TEXT NOT NULL,
+    calificacion TEXT NOT NULL,
+    ciudad TEXT NOT NULL,
+    pais TEXT NOT NULL,
+    tesoro TEXT NOT NULL
+);
